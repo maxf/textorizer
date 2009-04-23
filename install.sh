321571d53e37fcac662c575158e9178ea03d82e5
Max Froumentin <max@lapin-bleu.net>
2009-04-23 15:07:44 +0200

    fix permissions and scp line

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ zip -r textorizer12.windows.zip application.windows
 zip -r textorizer12.macosx.zip application.macosx
 zip -r textorizer12.linux.zip application.linux
 
-scp *.zip lapin-bl.eu/textorizer12
+scp *.zip dreamhost:lapin-bl.eu/textorizer12
