d2c9ac9af4fe53dd5381add18457777e7fb3d5f2
Max Froumentin <max@lapin-bleu.net>
2009-04-24 14:28:13 +0200

    use symbolic links for latest version

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 DATESTRING=`date "+%Y%m%d"`
+rm textorizer-*.zip
 cp Textorizer.icns application.macosx/textorizer.app/Contents/Resources
 cp Textorizer.icns application.macosx/textorizer.app/Contents/Resources/sketch.icns
 zip -r textorizer-windows-${DATESTRING}.zip application.windows
-zip -r textorizer-macosx--${DATESTRING}.zip application.macosx
+zip -r textorizer-macosx-${DATESTRING}.zip application.macosx
 zip -r textorizer-linux-${DATESTRING}.zip application.linux
+ln -s  textorizer-linux-${DATESTRING}.zip textorizer-linux.zip
+ln -s  textorizer-macosx-${DATESTRING}.zip textorizer-macosx.zip
+ln -s  textorizer-windows-${DATESTRING}.ziptextorizer-windows.zip
 
-scp *.zip *.png dreamhost:lapin-bleu.net/software/textorizer
+rsync -va *.zip *.png dreamhost:lapin-bleu.net/software/textorizer
