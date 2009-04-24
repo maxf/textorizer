6805223ec41f4d17c29d752f31a9cb7acdf1637c
Max Froumentin <max@lapin-bleu.net>
2009-04-24 14:23:13 +0200

    add date to generated zips

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
+DATESTRING=`date "+%Y%m%d"`
 cp Textorizer.icns application.macosx/textorizer.app/Contents/Resources
 cp Textorizer.icns application.macosx/textorizer.app/Contents/Resources/sketch.icns
-zip -r textorizer.windows.zip application.windows
-zip -r textorizer.macosx.zip application.macosx
-zip -r textorizer.linux.zip application.linux
+zip -r textorizer-windows-${DATESTRING}.zip application.windows
+zip -r textorizer-macosx--${DATESTRING}.zip application.macosx
+zip -r textorizer-linux-${DATESTRING}.zip application.linux
 
 scp *.zip *.png dreamhost:lapin-bleu.net/software/textorizer
